34871e5d6052cf2a94f6188359b77e4759a9474a
viks-iitj <g24ai2004@iitj.ac.in>
2025-07-06 15:28:27 +0530

    updated working code- v1

--- myApp/admin.py
+++ myApp/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, SubCategory, Product
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    search_fields = ['name']
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'category']
+    search_fields = ['name']
+    list_filter = ['category']
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'sub_category', 'price', 'rating', 'reviews']
+    search_fields = ['title']
+    list_filter = ['category', 'sub_category']
--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    title = models.CharField(max_length=300)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    image_url = models.URLField(max_length=500, blank=True, null=True)
+    price = models.FloatField()
+    rating = models.FloatField()
+    reviews = models.IntegerField()
+    about = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
